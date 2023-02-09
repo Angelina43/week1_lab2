b1cf028b853aa262fefe4edc324582374f9e77b7
angelina43 <bushueva.03@inbox.ru>
2023-02-09 11:13:01 +0700

    Создан массив для сравнения длины

--- js/main.js
+++ js/main.js
@@ -5,33 +5,45 @@ Vue.component('cards1', {
     props: {
         column1: {
             type: Array
-        },
+        }
     },
         template: `
          <div class="new">
         <ul v-for="(cd, index) in column1" :key="index">
             <li><p>{{ cd.name }}</p></li>
-            <p>{{ cd.task1 }}</p>
-            <p>{{ cd.task2 }}</p>
-            <p>{{ cd.task3 }}</p>
+            <p>{{ cd.task1 }} <button @click="task1_del(index)">Клик 1</button></p>
+            <p>{{ cd.task2 }}<button @click="task2_del(index)">Клик 2</button></p>
+            <p>{{ cd.task3 }}<button @click="task3_del(index)">Клик 3</button></p>
             <button @click="card2_del(index)">Перенести во 2 столбец</button>
         </ul>
         </div>
     `,
-    //     methods: {
-    //         // removeTask: function(task1){
-    //         //     let filtersList = this.tasks.filter(element => element !== task1)
-    //         //     this.tasks=filtersList
-    //         // },
-    //         // removeTask: function (index){
-    //         //     this.tasks.splice(index,1)
-    //         // },
+    data() {
+        return{
+            task: []
+        }
+    },
     methods: {
         card2_del(id){
             let desk = this.column1.splice(id,1)
             let desk2 = desk.pop()
             this.$emit('card2_trans', desk2);
-        }
+        },
+        task1_del(index){
+            let task1 = this.column1[index = 0].task1
+            this.task.push(task1)
+            console.log(this.task)
+        },
+        task2_del(index){
+            let task2 = this.column1[index = 0].task2
+            this.task.push(task2)
+            console.log(this.task)
+        },
+        task3_del(index){
+            let task3 = this.column1[index = 0].task3
+            this.task.push(task3)
+            console.log(this.task)
+        },
     }
 })
 
@@ -106,7 +118,7 @@ let app = new Vue({
             task1: '',
             task2: '',
             task3: '',
-        }
+        },
     },
     methods: {
         addCard(){
@@ -119,6 +131,10 @@ let app = new Vue({
                         task3: this.formCard.task3,
                     }
                 )
+                this.formCard.name = null
+                this.formCard.task1 = null
+                this.formCard.task2 = null
+                this.formCard.task3 = null
             }
             else {
                 alert('Превышен ввод')
